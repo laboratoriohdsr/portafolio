3628cd26b6bf811318c0ac24ade999f67043e987
laboratoriohdsr <laboratoriohdsr@gmail.com>
2026-07-31 02:31:36 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
      siempre en vivo del servidor.
    =========================================================== */
 
-const VERSION = 'portafolio-lab-v9';
+const VERSION = 'portafolio-lab-v10';
 const CACHE_APP = `${VERSION}-app`;
 const CACHE_EST = `${VERSION}-estaticos`;
 
